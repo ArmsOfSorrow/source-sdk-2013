6ec965a8c3210b8c36f19f04e39d4fbc0ea13c4f
ArmsOfSorrow <wave.dev@hotmail.com>
2014-04-08 00:04:07 +0200

    Added client side scanvisor implementations.
    Some fixes are still needed, though; for instance, the view should not
    lock if the player does not have a scannable target.

---
+++ sp/src/game/client/c_weapon_scanvisor.h
@@ -0,0 +1,23 @@
+#include "cbase.h"
+#include "c_basehlcombatweapon.h"
+#include "c_weapon__stubs.h"
+
+
+class C_WeaponScanvisor : public C_BaseHLCombatWeapon
+{
+public:
+	DECLARE_CLASS(C_WeaponScanvisor, C_BaseHLCombatWeapon);
+	DECLARE_CLIENTCLASS();
+	DECLARE_PREDICTABLE();
+	/*C_WeaponScanvisor();*/
+	//void ClientThink();
+
+	bool m_bIsCurrentlyScanning;
+	//client think for updating convar
+};
+
+STUB_WEAPON_CLASS_IMPLEMENT(weapon_scanvisor, C_WeaponScanvisor);
+
+IMPLEMENT_CLIENTCLASS_DT(C_WeaponScanvisor, DT_WeaponScanvisor, CWeaponScanvisor)
+RecvPropBool(RECVINFO(m_bIsCurrentlyScanning))
+END_RECV_TABLE()
---
+++ sp/src/game/server/weapon_scanvisor.h
@@ -0,0 +1,46 @@
+//==================================================//
+// Author: ArmsOfSorrow
+// Purpose: A weapon to mimic Metroid Prime's scanning 
+//			mechanic
+//==================================================//
+
+
+#include "basehlcombatweapon.h"
+#include "basecombatweapon_shared.h"
+#include "basecombatcharacter.h"
+#include "in_buttons.h"
+#include "playerlocaldata.h"
+#include "util.h"
+#include "usercmd.h"
+#include "const.h"
+
+#define SCAN_TIME_NORMAL 3.0f
+#define SCAN_RANGE_NORMAL 512 //test range, might change during balancing (if I ever come that far)
+
+class CWeaponScanvisor : public CBaseHLCombatWeapon
+{
+	DECLARE_CLASS(CWeaponScanvisor, CBaseHLCombatWeapon);
+
+public:
+	CWeaponScanvisor();
+	void PrimaryAttack();
+	void ItemPreFrame();
+	void DryFire();
+	int UpdateTransmitState();
+
+	CNetworkVar(bool, m_bIsCurrentlyScanning);
+
+	DECLARE_SERVERCLASS();
+	DECLARE_DATADESC();
+
+private:
+	float m_flScanTime;
+	int m_nOldButtonState;
+
+	CBasePlayer *m_pPlayer;
+	CBaseEntity *m_pTarget; //this should be an own type, to vary scanning times
+
+	void AcquireTarget();
+	void LockOnTarget(CBaseEntity *pEnt);
+};
+
